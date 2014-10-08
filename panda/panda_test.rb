528988e293df5cab886af04f1809e16108b2651f
Jesse Wolgamott <jesse@comalproductions.com>
2014-10-08 12:54:00 -0500

    Pandas actually do not sleep all that much #themoreyouknow

--- panda/panda_test.rb
+++ panda/panda_test.rb
@@ -5,25 +5,35 @@ require './panda'
 class PandaTest < MiniTest::Test
 
   def test_panda_eats_bamboo_pikas_rodent
-    panda = Panda.new
     %i(bamboo pikas rodent).each do |food|
       assert_equal true, panda.eat(food)
     end
   end
 
   def test_panda_does_not_eat_bacon
-    panda = Panda.new
     assert_equal false, panda.eat(:bacon)
   end
 
   def test_panda_climbs_trees
-    panda = Panda.new
     assert_equal true, panda.climb(:tree)
   end
 
   def test_panda_does_not_climbs_hill
-    panda = Panda.new
     assert_equal false, panda.climb(:hill)
   end
+
+  def test_pandas_do_not_sleep_a_bunch
+    percent = panda.sleep_percentage
+    assert_equal false, (percent >= bunch_threshold)
+  end
+
+  def bunch_threshold
+    16/24.0
+  end
+
+  def panda
+    Panda.new
+  end
+  
 
 end
